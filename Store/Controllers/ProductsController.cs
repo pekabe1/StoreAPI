a671c89e8a9453ce787cbb15c36f8f38800a2869
k.ziarkowski <k.ziarkowski@PK3-3>
2019-11-18 18:06:59 +0100

    Add project files.

---
+++ Store/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Store.Controllers
+{
+   // [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet("api/products/{name?}")]
+        public IActionResult GetAll(string name)
+        {
+            return Ok($"Test {name}");
+        }
+    }
+}
